183c69a76a61bacb87bfc9b0d17c3f2d6a03377e
luomai <luomai@localhost>
2014-05-06 14:22:37 +0800

    commit

--- CardDemo/gen/com/android/vending/billing/IInAppBillingService.java
+++ CardDemo/gen/com/android/vending/billing/IInAppBillingService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/luomai/Documents/other/cardslib-master/demo/stock/src/main/src/com/android/vending/billing/IInAppBillingService.aidl
+ * Original file: /Users/luomai/git/Cardlibdemo/CardDemo/src/com/android/vending/billing/IInAppBillingService.aidl
  */
 package com.android.vending.billing;
 /**
